6c2d9d1df8e15954a0032274f22c0714f44a325b
CIntellifusion <96377955+CIntellifusion@users.noreply.github.com>
2023-02-05 13:11:21 +0800

    更新了数据预处理任务

---
+++ code/utensil/Readme.md
@@ -0,0 +1,69 @@
+
+
+## 文本预处理
+
+> 洗数据  <br>
+>
+> 先空值列清除 bk<br>
+>
+> is_private 删除 ——bk<br>
+>
+> crawl time、d 删除 --bk<br>
+>
+> // dict 里面 怎么样批量选择 Keys （遍历选择） 
+>
+> // 新建空列表 二维列表  里面的一维 append(key-value)
+>
+> 
+>
+> 
+>
+> 计算 content - texts 列同时为空的情况 --why 
+>
+>  'basics_text': 501696,
+>
+>  'judge_record_text': 877445,
+>
+>  'head_text': 889256,
+>
+>  'tail_text': 888968,
+>
+>  'judge_result_text': 844359,
+>
+>  'judge_reson_text': 843756,
+>
+>  'content': 889502,
+>
+> u3000清理一下， \n\n\n\n清理
+>
+> 1125799->23W
+>
+> 
+>
+> --xby 
+>
+> 看一下每一列有多少个取值
+>
+> court_id head
+>
+> apellor 去重
+>
+> keywords 去none
+>
+> case_type
+>
+> cause
+>
+> trial_round 
+>
+> cause
+>
+> settle_type
+>
+> // set([a.b.c.d..dd]) list.unique()
+>
+> // len(set) <10 
+>
+> // sorted(['a'’‘’‘])
+>
+> // a[i]->i 
--- code/utensil/get_text.py
+++ code/utensil/get_text.py
@@ -9,15 +9,23 @@ class Case():
             self.file = None
         self.line = 0
         #print(self.line)
-        columns = np.load("./no0log.npy",allow_pickle=True).item()
-        columns =  list(columns.keys())
+    # 每次从file里面读取1行，返回 行号和字典 
+    # 注意 不可重入。 避免重入的方法是： 重新初始化这个类
     def get_dict(self):
         file = self.file
         suit = file.readline()
         dictionary = json.loads(suit)
         self.line+=1
-        #print(self.line)
-        #print("here")
         return self.line-1,dictionary
+    # esay to understand 
     def close(self):
         self.file.close()
+    # 加载 一个非0 的数据列字典  
+    def load_log(self,colpath):
+        columns = np.load(colpath,allow_pickle=True).item()
+        self.columns =  columns
+    # 增加一个成员函数
+    def function1(self):
+        whiel()
+        self.get_()
+        #每次处理一行
