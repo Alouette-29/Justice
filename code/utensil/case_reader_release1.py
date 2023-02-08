5f578cd48bfb2ed47ee28d6ff9aa29b12a22b767
CIntellifusion <96377955+CIntellifusion@users.noreply.github.com>
2023-02-08 13:18:51 +0800

    数据预处理第一版

--- code/utensil/case_reader_release1.py
+++ code/utensil/case_reader_release1.py
@@ -6,36 +6,36 @@ import json
 import os 
 import numpy as np 
 import pandas as pd 
-# 注意  所有的文件名都传参了  ，为了方便修改 
+# 写代码之前默念： 高内聚低耦合 十遍
+# 版本： release 1 
+# 发布时间： 2023/2/8
+# 注意  所有的文件名都传参了 ，为了方便修改 
 class Case():
     def __init__(self,filename='../p2-1-2021.txt',encoding='utf-8',colpath = '../keys_without_0.npy') -> None:
         # 这个主要是数据清洗的时候要用
         # 因为我们可以从文件名打开文件，而不调用self.get_dict()
         self.filename = filename 
-         # xby修改=====
-        self.dictionary = {}
         if os.path.exists(filename) and os.path.isfile(filename):
             self.file = open(filename,"r",encoding=encoding)
         else:
             self.file = None
         self.line = 0
-        self.dictionary = {}
-        self.ttline = 1125799
-        #self.ttline = 0 
+        self.ttline = 0
         self.columns = self.load_log(colpath)
         self.get_ttline()
-        #print(self.line)
+        self.writelines = 0
     # 每次从file里面读取1行，返回 行号和字典 
     # 注意 不可重入。 避免重入的方法是： 重新初始化这个类
     def get_ttline(self):
         if self.ttline != 0 :
             # 说明这个函数已经执行过了 
-            return 
-        storage = os.stat(self.filename).st_size
+            return
+        filesize = os.stat(self.filename).st_size
         read = 0
         buffer_size = 1024*8192
         thefile=open(self.filename,encoding='utf-8')
         start_time = time()
+       
         while True:
             buffer=thefile.read(buffer_size)
             if not buffer:
@@ -43,7 +43,7 @@ class Case():
             read += buffer_size
             self.ttline+=buffer.count('\n')
             crt_timer = time()
-            print("\r %.2f has been counted,%.2f seconds has been taken "%((read/storage),(crt_timer-start_time)) ,end="")
+            print("\r %.2f has been counted,%.2f seconds has been taken "%((read/filesize),(crt_timer-start_time)) ,end="")
         thefile.close()
         print("Total lines of the file is ",self.ttline)
         return 
@@ -57,45 +57,42 @@ class Case():
     # esay to understand 
     def close(self):
         self.file.close()
-    # 加载 一个非0 的数据列字典  
+    # 加载 非0 的数据列字典  
     def load_log(self,colpath):
         columns = np.load(colpath,allow_pickle=True).item()
         self.columns =  columns
-        #print(self.columns)
         return self.columns
     @staticmethod
     def dict2string(dictionary,filename=None):
-        # string = json.dumps(dict)
-        # string = re.sub('\'',"\"",str(dictionary))
-        # string = re.sub("None","null",str(string))
-        # dumps 不行 不好做标注啊我草 
-        string = json.dumps(dictionary)
+        # 可能因为引号会有一些问题 
+        string = re.sub('\'',"\"",str(dictionary))
+        string = re.sub("None","null",string)
+        # string = json.dumps(dictionary)
         string+='\n'
         if filename!=None:
             with open(filename,'a',encoding='utf-8') as f:
                 f.write(string)
         return string
-    def appellor_dedup(self,dictionary):
+    @staticmethod
+    def appellor_dedup(dictionary):
         item = dictionary['appellor']
         item = item.split(",")
-        dictionary['appellor']= list(set(item))
+        #传进来是字符串，传出去也是字符串 
+        dictionary['appellor']= ",".join(list(set(item)))
         return dictionary
     @staticmethod
     def regulize(case_string):
-    #处理逻辑
-    # part2 wash content
+    # 正则化 一个字符串 
     # 将连续的 \n转换成 一个\n  
-        import re
-        #print(type(case_string))
-        pattern1 = re.compile("[ \t\r\f]+")# 匹配除了\n的连续空白符号 
+        pattern1 = re.compile("\s+")# 匹配除了\n的连续空白符号 
         pattern2 = re.compile('\n+') # 匹配连续的加号
         pattern3 = re.compile("\u3000|\\xa0+") #清除奇怪的符号
+        pattern4 = re.compile("([a-zA-Z]+=.*?>)") # 去匹配没有洗掉的html格式 
         result = re.sub(pattern1,'',case_string)
         result = re.sub(pattern3,'',result)
         result = re.sub(pattern2,'\n',result)
+        result = re.sub(pattern4,'',result)
         return result
-    # 以下为ybk的函数的实验版,功能是去除某些键值对。注意:要实现多人代码的流水线协作，应该传入一个olddict
-    #@staticmethod
     def delete_none_value(self,olddict):
         #经测试，顺序删除原字典中的键值对会报错，原因是每次执行后字典长度发生改变。所以考虑用新字典来存放。
         #olddict=self.get_dict()[1] #取得原字典
@@ -107,43 +104,6 @@ class Case():
                 and k[0]!='is_private' and k[0]!='crawl_time' and k[0]!='d':  #在对照字典里，添加进去
                 newdict[k[0]] = k[1]
         return newdict  #返回值为新的字典
-    def modify_courtname(self,olddict,oldframe):
-        str=olddict['court_name']
-        ls=[]
-        dt = {}
-        slen = len(str)
-        id1 = str.find('省')
-        if (id1!=-1):
-            dt['省'] = str[0:id1 + 1]
-        id2 = str.find('市')
-        #print(id2)
-        if (id2 != -1):
-            dt['市'] = str[id1 + 1:id2 + 1]
-        else:
-            id2=str.find('自治州')+2
-            if(id2!=-1):
-                dt['市']=str[id1+1:id2+1]
-        idm1=max(id1,id2)
-        id3=str.find('区')
-        if(id3!=-1):
-            dt['区']=str[idm1+1:id3+1]
-            dt['地址']=str[id3+1:slen]
-        else:
-            id3=id2
-            dt['地址']=str[id3+1:slen]
-
-        #id=olddict['court_id'] #adcode
-        province=dt.get('省') if dt.get('省')!=None else '' #省
-        city=dt.get('市') if dt.get('市')!=None else '' #市
-        district=dt.get('区')if dt.get('区')!=None else '' #区
-        loc=dt.get('地址')if dt.get('地址')!=None else '' #地址
-        ls.append(province)
-        ls.append(city)
-        ls.append(district)
-        ls.append(loc)
-        #ls.append(id)
-        df2= pd.DataFrame(np.insert(oldframe.values, len(oldframe.index), values=ls, axis=0)) #更新
-        return df2
     @staticmethod
     def get_boolean(dictionary,boolean_df = "None_relation_table.txt"):
         # 用来说明 content 为 none 的时候，其他字段 也为None  
@@ -157,7 +117,7 @@ class Case():
             dictionary['judge_reson_text']==None]
         df = pd.DataFrame(boolarray).T
         df.to_csv(boolean_df,mode = 'a',header=None,index=None)
-    def preprocess(self,clean_set = "../cleanset.txt",string=None):
+    def preprocess(self,clean_set = "../cleanset.txt",string=None,wrong_log ="../wrong_logs.txt"):
         # 这是总的预处理函数，负责提供遍历数据集的骨架
         # 下面是子函数 ，在不同的分支调用不同的处理函数
         # 函数逻辑是 按行处理  行间互不干扰
@@ -175,10 +135,11 @@ class Case():
                 return 
             dicts['content'] = self.regulize(dicts['content'])
             result_content = self.dict2string(dicts)
-            json.loads(result_content)
-            return result_content
+            
+            return json.loads(result_content)
         with open(self.filename,"r",encoding="utf-8") as f:
             # 一些统计量 
+            no_legal = 0
             writelines = 0 
             for line in tqdm(range(self.ttline)):
                 content = f.readline()
@@ -195,7 +156,6 @@ class Case():
                     # 此处应该统计 与其他六个字段为None 的关系 
                     self.get_boolean(dicts)
                 else:
-                    writelines+=1
                     # 如果这一行不为空
                     # 那么我们需要 删掉空字段 
                     # 并且对其去重 
@@ -204,38 +164,36 @@ class Case():
                     dicts = self.appellor_dedup(dicts)
                     dicts['content'] = self.regulize(dicts['content'])
                     result_string= self.dict2string(dicts)
-                    with open(clean_set,'a',encoding='utf-8') as file:
-                        file.write(result_string)
+                    
+                    legal = self.test_preprocess(result_string)
+                    if legal:
+                        with open(clean_set,'a',encoding='utf-8') as file:
+                            file.write(result_string)
+                            writelines+=1
+                    else:
+                        with open(wrong_log,'a',encoding='utf-8') as file:
+                            file.write(result_string)
+                            no_legal+=1
+                    
+            
+        print("%d is written to cleanset, %d is wrong" % (writelines,no_legal))
                 #途中的输出  预计加一个进度条  提高一下代码质量 
                 # if(writelines%10000==0):
-                #     break
+                #      break
+                
                 #     crt_timer = time()
                 #     # if(crt_timer-pre_timer<0.05):
                 #     #     continue
                 #     # 上面这段改成直接 直接修改这一行的输出就行了 
                 #     print("\r%d lines has been writed %.2f seconds has been used， average speech : %.2f" % (writelines,crt_timer-pre_timer,(crt_timer-pre_timer)/(writelines/10000)),end="")
         self.writelines = writelines      
-
-    def test_preprocess(self,clean_set = "../cleanset.txt"):
-        file = open(clean_set,'r',encoding='utf-8')
-        errnum=0 
-        wronglog_file = "./wronglog.txt"
-        for i in tqdm(range(self.writelines)):
-            content = file.readline()
-            if content==None:
-                break
-            try:
-                json.loads(content)
-            except:
-                errnum+=1
-                print("\r %d line is wrong "% errnum,end="")
-                with open(wronglog_file,'a',encoding='utf-8') as f:
-                    f.write(content)
-        if errnum==0:
-            print("All cases checked, preprocess success")
-        else:
-            print("%d line is wrong "% errnum,end="")
-    # 现在还差一些统计函数
+    @staticmethod
+    def test_preprocess(dicts):
+        try:
+            json.loads(dicts)
+            return 1
+        except:
+            return 0    # 现在还差一些统计函数
     # string->value 
     # split geographical information 
     # 
@@ -292,7 +250,7 @@ def cal_matrix(col:int,reports,total:dict):
 
 
 if __name__=='__main__':
-    reader = Case('../p2-1-2021/p2-1-2021.txt')
-    #reader.preprocess()
+    reader = Case('../dataset/p2-1-2021.txt')
+    reader.preprocess()
     reader.test_preprocess()
-    analysis_boolarray("./None_relation_table.txt")
+    #analysis_boolarray("./None_relation_table.txt")
